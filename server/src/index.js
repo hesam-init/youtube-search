0b5f3c802c06459099dd8a927f86271bf21f2663
h.yosefpour <hesam.init@gmail.com>
2022-05-31 16:36:23 +0430

    [UPDATE_SERVER] :  serve static build

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 // node modules:
 import express from "express";
+import path, { dirname } from "path";
 import searchApi from "./search.js";
 
 // express config:
@@ -10,17 +11,37 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// express routes:
-app.get("/", async (req, res) => {
+// server route:
+const buildPath = path.resolve(dirname("./"), "../client/build");
+app.use(express.static(buildPath));
+
+const indexPath = path.resolve(dirname("./"), "../client/build/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(indexPath);
+});
+
+// api routes:
+app.get("/api", async (req, res) => {
   let data = await searchApi(req.query.name || "");
   if (data.length === 0) {
-    res.status(404).send("no results found use ?name=<videoname>");
+    res.status(404).send("no results found use, ?name=<videoname>");
   } else {
+    return res.status(200).send(data);
+  }
+});
+
+// getlink route:
+app.get("/api/link", async (req, res) => {
+  let data = req.query.link;
+  if (data.length === 0) {
+    res.status(404).send("no results found use, ?link=<videoname>");
+  } else {
+    console.log(req.query.link);
     return res.status(200).send(data);
   }
 });
 
 // express server:
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`app serve on : http://localhost:${port}`);
 });
