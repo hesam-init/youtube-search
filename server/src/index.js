3260183e81c2f16c6b3a8fcdf4f02e2ac795410c
hesam-init <hesam.init@gmail.com>
2022-06-05 22:26:07 +0430

    [UPDATE_SERVER] : image delivery

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 // node modules:
 import cors from "cors";
+import axios from "axios";
 import express from "express";
 import searchApi from "./search.js";
 import path, { dirname } from "path";
@@ -35,14 +36,18 @@ app.get("/api", async (req, res) => {
   }
 });
 
-// getlink route:
-app.get("/api/link", async (req, res) => {
-  let data = req.query.link;
-  if (data.length === 0) {
-    res.status(404).send("no results found use, ?link=<videoname>");
-  } else {
-    console.log(req.query.link);
-    return res.status(200).send(data);
+// send image by url:
+app.get("/api/images", async (req, res) => {
+  let link = req.query.link;
+  try {
+    const response = await axios({
+      method: "GET",
+      url: link,
+      responseType: "stream",
+    });
+    response.data.pipe(res);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
